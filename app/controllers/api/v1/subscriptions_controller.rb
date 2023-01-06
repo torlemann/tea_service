285e258cb77d6372396615ab3f46dd471557051f
Taryn Orlemann <taryn.orlemann@gmail.com>
2023-01-06 10:52:03 -0500

    WIP create subs

---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::SubscriptionsController < ApplicationController
+    def create
+        subscription = Subscription.new(sub_params)
+        if subscription.save
+          render json: subscription
+        else
+          render json: { error: subscription.errors.full_messages.to_sentence }
+        end
+    end
+  
+    private
+  
+    def sub_params
+      params.permit(:title, :price, :status, :frequency, :tea_id, :customer_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get "/subscriptions/:customer_id", to: 'subscriptions#index'
+      post "/subscriptions", to: 'subscriptions#create'
+      delete "/subscriptions/:customer_id", to: 'subscriptions#destroy'
+    end
+  end
 end
