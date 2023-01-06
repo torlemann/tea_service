e52f2aa8e023720ce03dbbb39e327a3b85c497cf
Taryn Orlemann <taryn.orlemann@gmail.com>
2023-01-06 14:45:14 -0500

    Test: get all subs by customer id

--- spec/requests/api/v1/subscriptions/subscription_request_spec.rb
+++ spec/requests/api/v1/subscriptions/subscription_request_spec.rb
@@ -5,55 +5,75 @@ RSpec.describe "Subscription" do
     describe 'create' do
         it 'can create an tea subscription for a customer' do
 
-        customer_1 = Customer.create!(first_name: "Ralph", last_name: "Piccarro", email: "shmoly@shmomail.com", street_address: "1400 Dis Street")
-        tea_1 = Tea.create!(title: "Lapsang Souchong", description: "dark and smokey", temperature: 180, brew_time: 5)
+            customer_1 = Customer.create!(first_name: "Ralph", last_name: "Piccarro", email: "shmoly@shmomail.com", street_address: "1400 Dis Street")
+            tea_1 = Tea.create!(title: "Lapsang Souchong", description: "dark and smokey", temperature: 180, brew_time: 5)
 
-        sub_params = {
-            title: "#{customer_1.first_name} #{customer_1.last_name}'s #{tea_1.title} Sub",
-            price: 10,
-            status: 1,
-            frequency: 2,
-            tea_id: tea_1.id,
-            customer_id: customer_1.id
-        }
+            sub_params = {
+                title: "#{customer_1.first_name} #{customer_1.last_name}'s #{tea_1.title} Sub",
+                price: 10,
+                status: 1,
+                frequency: 2,
+                tea_id: tea_1.id,
+                customer_id: customer_1.id
+            }
 
-        headers = {"CONTENT_TYPE" => "application/json"}
+            headers = {"CONTENT_TYPE" => "application/json"}
 
-        post "/api/v1/subscriptions", headers: headers, params: JSON.generate(sub_params)
+            post "/api/v1/subscriptions", headers: headers, params: JSON.generate(sub_params)
 
-        expect(response).to be_successful
+            expect(response).to be_successful
 
-        subscription = JSON.parse(response.body, symbolize_names: true)
+            subscription = JSON.parse(response.body, symbolize_names: true)
 
-        expect(subscription).to be_a Hash
-        expect(subscription).to have_key(:data)
-        expect(subscription[:data]).to have_key(:id)
-        expect(subscription[:data]).to have_key(:type)
-        expect(subscription[:data]).to have_key(:attributes)
-        expect(subscription[:data][:attributes]).to be_a Hash
-        expect(subscription[:data][:attributes].keys).to eq([:title, :price, :status, :frequency, :tea_id, :customer_id])
+            expect(subscription).to be_a Hash
+            expect(subscription).to have_key(:data)
+            expect(subscription[:data]).to have_key(:id)
+            expect(subscription[:data]).to have_key(:type)
+            expect(subscription[:data]).to have_key(:attributes)
+            expect(subscription[:data][:attributes]).to be_a Hash
+            expect(subscription[:data][:attributes].keys).to eq([:title, :price, :status, :frequency, :tea_id, :customer_id])
         end
     end
 
     describe 'cancel' do
         it 'can cancel an tea subscription for a customer' do
 
-        customer_1 = Customer.create!(first_name: "Ralph", last_name: "Piccarro", email: "shmoly@shmomail.com", street_address: "1400 Dis Street")
-        tea_1 = Tea.create!(title: "Lapsang Souchong", description: "dark and smokey", temperature: 180, brew_time: 5)
+            customer_1 = Customer.create!(first_name: "Ralph", last_name: "Piccarro", email: "shmoly@shmomail.com", street_address: "1400 Dis Street")
+            tea_1 = Tea.create!(title: "Lapsang Souchong", description: "dark and smokey", temperature: 180, brew_time: 5)
 
-        subscription_1 = Subscription.create!(
-            title: "#{customer_1.first_name} #{customer_1.last_name}'s #{tea_1.title} Sub",
-            price: 10,
-            status: 1,
-            frequency: 2,
-            tea_id: tea_1.id,
-            customer_id: customer_1.id
-        )
-        headers = { 'CONTENT_TYPE' => 'application/json' }
+            subscription_1 = Subscription.create!(
+                title: "#{customer_1.first_name} #{customer_1.last_name}'s #{tea_1.title} Sub",
+                price: 10,
+                status: 1,
+                frequency: 2,
+                tea_id: tea_1.id,
+                customer_id: customer_1.id
+            )
+            headers = { 'CONTENT_TYPE' => 'application/json' }
 
-        delete "/api/v1/subscriptions/#{subscription_1.id}", headers: headers, params: JSON.generate(subscription_1)
+            delete "/api/v1/subscriptions/#{subscription_1.id}", headers: headers, params: JSON.generate(subscription_1)
 
-        expect(response).to be_successful
+            expect(response).to be_successful
         end
     end
+
+    describe 'index' do 
+        it 'returns all active and inactive subscriptions' do 
+            customer_1 = Customer.create!(first_name: "Ralph", last_name: "Piccarro", email: "shmoly@shmomail.com", street_address: "1400 Dis Street")
+            tea_1 = Tea.create!(title: "Lapsang Souchong", description: "dark and smokey", temperature: 180, brew_time: 5)
+
+            subscription_1 = Subscription.create!(
+                title: "#{customer_1.first_name} #{customer_1.last_name}'s #{tea_1.title} Sub",
+                price: 10,
+                status: 1,
+                frequency: 2,
+                tea_id: tea_1.id,
+                customer_id: customer_1.id
+            )
+            headers = { 'CONTENT_TYPE' => 'application/json' }
+            get "/api/v1/subscriptions/#{customer_1.id}", headers: headers, params: JSON.generate(customer_1.id)
+    
+            expect(response).to be_successful
+        end
+      end 
 end
